4682994f13ef967230e312d80f905d8656faacf9
TongguangZhang <tonggz250@gmail.com>
2025-06-07 00:08:09 -0700

    remove useless div

--- camera_tuning/tuning_ui/src/modules/calibrationModule.tsx
+++ camera_tuning/tuning_ui/src/modules/calibrationModule.tsx
@@ -4,29 +4,28 @@ import ImageStream from "../components/imageStream"
 
 const CalibrationModule = () => {
     return (
-        <div>
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                gap: "20px",
+            }}
+        >
             <p>Camera Tuning UI</p>
+
+            <CalibrateCard />
             <div
                 style={{
                     display: "flex",
-                    flexDirection: "column",
+                    justifyContent: "center",
                     alignItems: "center",
                     gap: "10px",
                 }}
             >
-                <CalibrateCard />
-                <div
-                    style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        gap: "10px",
-                    }}
-                >
-                    <CaptureButton url="http://localhost:8085/capture/capture" text="Capture" />
-                    <CaptureButton url="http://localhost:8085/capture/save" text="Save" />
-                    <CaptureButton url="http://localhost:8085/capture/clear" text="Clear" />
-                </div>
+                <CaptureButton url="http://localhost:8085/capture/capture" text="Capture" />
+                <CaptureButton url="http://localhost:8085/capture/save" text="Save" />
+                <CaptureButton url="http://localhost:8085/capture/clear" text="Clear" />
             </div>
             <ImageStream />
         </div>
