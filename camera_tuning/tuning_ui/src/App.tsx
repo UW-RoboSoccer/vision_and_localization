8e46c067d79b4939891795a7f9ee03cc8ec74603
TongguangZhang <tonggz250@gmail.com>
2025-06-07 00:01:58 -0700

    update app to use calibrate module

--- camera_tuning/tuning_ui/src/App.tsx
+++ camera_tuning/tuning_ui/src/App.tsx
@@ -1,39 +1,10 @@
 import "./App.css"
-import CaptureButton from "./components/captureButton"
-import ImageStream from "./components/imageStream"
+import CalibrationModule from "./modules/calibrationModule"
 
 function App() {
     return (
         <>
-            <div>
-                <p>Camera Tuning UI</p>
-                <div
-                    style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                        gap: "10px",
-                    }}
-                >
-                    <CaptureButton
-                        url="http://localhost:8085/calibrate/calibrate"
-                        text="Calibrate Camera"
-                    />
-                    <div
-                        style={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            gap: "10px",
-                        }}
-                    >
-                        <CaptureButton url="http://localhost:8085/capture/capture" text="Capture" />
-                        <CaptureButton url="http://localhost:8085/capture/save" text="Save" />
-                        <CaptureButton url="http://localhost:8085/capture/clear" text="Clear" />
-                    </div>
-                </div>
-                <ImageStream />
-            </div>
+            <CalibrationModule />
         </>
     )
 }
