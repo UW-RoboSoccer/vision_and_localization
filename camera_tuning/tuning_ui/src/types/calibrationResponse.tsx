ffe906211f1b17afa5bfbf97e97cec9abc11a483
TongguangZhang <tonggz250@gmail.com>
2025-06-06 23:26:00 -0700

    calibrate result types

---
+++ camera_tuning/tuning_ui/src/types/calibrationResponse.tsx
@@ -0,0 +1,19 @@
+export interface CalibrationData {
+    ret_left: number
+    ret_right: number
+    ret_calibrate: number
+    K_left: number[][]
+    K_right: number[][]
+    D_left: number[][]
+    D_right: number[][]
+    R: number[]
+    T: number[]
+    Q: number[]
+    save_result_index: number
+}
+
+export interface CalibrationResponse {
+    error: Record<string, string>
+    data: CalibrationData
+}
+
