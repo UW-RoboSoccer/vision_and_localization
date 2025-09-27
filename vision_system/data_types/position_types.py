06ffe31b48fbc5570063f1abaaaf9557fd4ebb66
TongguangZhang <tonggz250@gmail.com>
2025-09-27 17:28:45 +0800

    setup

--- vision_system/data_types/position_types.py
+++ vision_system/data_types/position_types.py
@@ -1,13 +1,24 @@
 from pydantic import BaseModel
 
 
-class Position(BaseModel):
+class GlobalPosition(BaseModel):
     """
     Represents the position of an entity.
     Center dot is (0, 0).
     The x-axis from goal to goal, and the y-axis is from sideline to sideline.
     Our goal is negative x, enemy goal is positive x.
     Use ur brain to figure out the rest.
+    Units in m.
+    """
+
+    x: float
+    y: float
+
+class LocalPosition(BaseModel):
+    """
+    Represents the position of an entity with respect to the robot.
+    The robot is at (0, 0).
+    Positive x is right, positive y is forward.
     Units in m.
     """
 
